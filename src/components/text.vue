eca9b69f3d19dd2ecf104c0b3a039e89d26a2fb7
mlexperiencedavid@gmail.com <david@mlexperience.org>
2021-02-27 12:22:48 +0000

    adding text highlight feature

---
+++ src/components/text.vue
@@ -0,0 +1,38 @@
+<template>
+    <text-highlight :queries="queries">{{ description }}</text-highlight>
+
+</template>
+
+
+<script>
+export default {
+  name: 'text',
+  props: {
+    msg: String
+  },
+  data() {
+      return {
+          queries: ['birds','scatt'],
+          description: 'Tropical birds scattered as Drake veered the Jeep'
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import TextHighlight from 'vue-text-highlight'
 
+Vue.component('text-highlight', TextHighlight)
 Vue.config.productionTip = false
 
 new Vue({
